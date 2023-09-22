f3b0d1bda7d1b722ccb0609aea4ac436be11f63e
KaiqueRenanAlves143 <106277121+KaiqueRenanAlves143@users.noreply.github.com>
2023-09-22 08:17:46 -0300

    AULA 22/11

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,8 +1,48 @@
 alert('Flamengo');
 
-   document.querySelector('.tecla_pom');
+function tocaSomPom() { document.querySelector('.tecla_pom');
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-   document.querySelector('#som.tecla.pom').play();
+function tocaSomClap() { document.querySelector('.tecla_clap');
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim() { document.querySelector('.tecla_tim');
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff() { document.querySelector('.tecla_puff');
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash() { document.querySelector('.tecla_splash');
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim() { document.querySelector('.tecla_toim');
+}
+document.querySelector('.tecla_Toim').onclick = tocaSomToim;
+
+function tocaSomPsh() { document.querySelector('.tecla_psh');
+}
+document.querySelector('.tecla_Psh').onclick = tocaSomPsh;
+
+function tocaSomTic() { document.querySelector('.tecla_tic');
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom() { document.querySelector('.tecla_tom');
+}
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
+
+
+
+
+
+
+
+
 
 
 
